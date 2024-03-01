d93e20501539c1ea32ae8ffa4cffbc579e520c9e
monishkasagiraju <monishka.sagiraju@gmail.com>
2024-03-01 23:48:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+let sec = document.querySelector('.sec');
+let contentHeight = sec.offsetHeight - window.innerHeight;
+
+window.addEventListener('scroll', () => {
+    let value = Math.min(window.scrollY, contentHeight);
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+});
